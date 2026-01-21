e92425b42703e2edc7fcd2d7235943c37acc7ceb
WilliamTaing04 <William.Taing04@gmail.com>
2026-01-21 14:54:50 -0800

    created camera tag tracking(needs to be tested)

---
+++ Camera_Intrinsics_Calibration.py
@@ -0,0 +1,105 @@
+
+import cv2
+import numpy as np
+
+def setup():
+    cap = cv2.VideoCapture(1, cv2.CAP_MSMF)
+
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    cap.set(cv2.CAP_PROP_FPS, 60)
+    cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+    cap.set(cv2.CAP_PROP_FOCUS, 535)
+
+    if not cap.isOpened():
+        print("Failed to open camera with MSMF")
+        exit()
+
+    #fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
+
+    return cap
+
+# ===== Checkerboard parameters =====
+CHECKERBOARD = (7, 4)      # inner corners (cols, rows)
+SQUARE_SIZE = 0.0035      # meters (or any consistent unit)
+# ===== Calibration flags =====
+flags = (cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC | cv2.fisheye.CALIB_CHECK_COND | cv2.fisheye.CALIB_FIX_SKEW)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 1e-6)
+objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
+objp[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+objp *= SQUARE_SIZE
+objpoints = []  # 3D points
+imgpoints = []  # 2D points
+cameraIndex = 0
+
+def main():
+    images = []
+    cap = setup()
+    # Get images
+    count = 0
+    while count < 20:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        status_text = f"Measurements: {count}/{20}"
+        cv2.putText(frame, status_text, (10, 30), 
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, "Ready! Press 'q' to capture", (10, 60),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.imshow("MSMF Camera", frame)
+
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            images.append(frame)
+            count += 1
+            cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+    print("20 Images Captured")
+    for img in images:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        found, corners = cv2.findChessboardCorners(
+            gray,
+            CHECKERBOARD,
+            cv2.CALIB_CB_ADAPTIVE_THRESH +
+            cv2.CALIB_CB_FAST_CHECK +
+            cv2.CALIB_CB_NORMALIZE_IMAGE
+        )
+        if not found:
+            print(f"Skipping")
+            continue
+
+        corners = cv2.cornerSubPix(
+            gray,
+            corners,
+            (3, 3),
+            (-1, -1),
+            criteria
+        )
+
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+    N_OK = len(objpoints)
+    print(f"Using {N_OK} valid images")
+
+    rvecs = [np.zeros((1, 1, 3)) for _ in range(N_OK)]
+    tvecs = [np.zeros((1, 1, 3)) for _ in range(N_OK)]
+
+    ret, K, dist, rvecs, tvecs = cv2.calibrateCamera(
+        objpoints,
+        imgpoints,
+        gray.shape[::-1],
+        None,
+        None
+    )
+
+    print("K:\n", K)
+    print("Dist:\n", dist)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
--- Camera_Tag_Tracking.py
+++ Camera_Tag_Tracking.py
@@ -1,18 +1,28 @@
-"""
-Lab 6 Part 3: Validation of Camera-workspace Calibration - STARTER CODE
-Tracks an AprilTag and transforms its position from camera to workspace frame.
-EE 471: Vision-Based Robotic Manipulation
-(c) 2025 S. Farzan, Electrical Engineering Department, Cal Poly
-"""
 import numpy as np
 import cv2
-from time import time
+import time
+import pickle
+import AprilTags
+import matplotlib.pyplot as plt
+from pathlib import Path
 
-from AprilTags import AprilTags
+# Directory where this file lives
+HERE = Path(__file__).parent
 
+def save_to_pickle(data: dict, filename: str):
+    """Save data dictionary to a pickle file in the same folder as this script."""
+    path = HERE / filename
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
 
+def load_from_pickle(filename: str):
+    """Load data dictionary from a pickle file in the same folder as this script."""
+    path = HERE / filename
+    with open(path, "rb") as f:
+        return pickle.load(f)
+    
 
-def main():
+def record():
     """
     Main validation routine - tracks a tag and displays its position
     """
@@ -66,7 +76,6 @@ def main():
     
     # Set the validation tag size in millimeters
     # IMPORTANT: Measure your validation tag!
-    # TAG_SIZE = 96.5  # Update this value
     TAG_SIZE = 65  # Update this value
 
     print(f"\nValidation tag size: {TAG_SIZE} mm")
@@ -80,13 +89,23 @@ def main():
     print("Press 'q' to quit")
     print("="*60 + "\n")
     
-    counter = 0
+    # Define control timestep
+    dt = 0.01  # Control loop period in seconds
+    # Pre-allocate arrays for data collection (over-allocate for safety)
+    max_samples = 7200     # 60Hz => 60(samples/s)*120s = 7200samples
+    data_time = np.zeros(max_samples)                 # Time (s)
+    data_ee_pos = np.zeros((max_samples, 3))          # End-effector pose [x,y,z] (mm)
+    count = 0  # Sample counter
+
+    begining_time = time.time()  # Record start time for data colleciton
+
     
     # =====================================================================
     # MAIN TRACKING LOOP
     # =====================================================================
     while True:
-        
+        start_time = time.time()  # Replace with actual time
+
         # -----------------------------------------------------------------
         # STEP 1: CAPTURE FRAME
         # -----------------------------------------------------------------
@@ -108,15 +127,14 @@ def main():
         # STEP 3: PROCESS DETECTED TAGS
         # -----------------------------------------------------------------
         
-        # # Check if any tags were detected
-        # if len(tags) > 0:
-        for tag in tags:
+        # Check if any tags were detected
+        if len(tags) > 0:
 
             
-            # # Use the first detected tag
-            # tag = tags[0]
+            # Use the first detected tag
+            tag = tags[0]
             
-            # Hint: Use detector.get_tag_pose(tag.corners, intrinsics, TAG_SIZE)
+            # Use detector.get_tag_pose(tag.corners, intrinsics, TAG_SIZE)
             # Returns: (rotation_matrix, translation_vector)
             rot_matrix, trans_vector = detector.get_tag_pose(tag.corners, intrinsics, TAG_SIZE)
 
@@ -125,7 +143,7 @@ def main():
             if rot_matrix is not None and trans_vector is not None:
                 
                 # Extract position in camera frame (already in mm)
-                # Hint: Flatten trans_vector to get a 1D array of shape (3,)
+                # Flatten trans_vector to get a 1D array of shape (3,)
                 pos_camera = trans_vector.flatten()  # Replace with position array
 
                 # Create full 4x4 pose transformation from tag to camera
@@ -134,7 +152,7 @@ def main():
                 T_tag_to_cam[:3, 3] = trans_vector.reshape((3,))  # Replace with translation vector
                 
                 # Transform full pose to workspace frame
-                # Hint: Multiply T_cam_to_workspace @ T_tag_to_cam
+                # Multiply T_cam_to_workspace @ T_tag_to_cam
                 T_tag_to_workspace = T_cam_to_workspace @ T_tag_to_cam  # Replace with homogeneous coordinates (4x4)
                 
                 # Extract position and orientation in workspace frame
@@ -146,6 +164,13 @@ def main():
                 
                 # Calculate distance from camera
                 distance = np.linalg.norm(pos_workspace)  # Replace with distance
+
+                # Collect Data-----------------------------------------------------
+                if count < max_samples:
+                    data_time[count] = time.perf_counter() - begining_time  # Time stamps (s)
+                    data_ee_pos[count, :] = pos_workspace                  # End-effector pose [x,y,z] (mm)
+                    count += 1
+
 
                 # Draw detection on image
                 detector.draw_tags(color_frame, tag)
@@ -192,26 +217,6 @@ def main():
                     cv2.putText(color_frame, f"{lab}: {pos_workspace[i]:.1f} mm",
                                 (x, start_y + i*line_h),
                                 font, xyz_scale, (0, 255, 255), thickness, cv2.LINE_AA)
-
-
-                # Print coordinates periodically to terminal
-                if counter % PRINT_INTERVAL == 0:
-                    print("\n" + "-"*50)
-                    print(f"Tag ID: {tag.tag_id}")
-                    print(f"Distance from camera: {distance:.1f} mm")
-                    print("\nTag Orientation:")
-                    # Print Tag Orientation in workspace Frame
-                    print(euler_workspace)
-                    
-                    print("\nCamera Frame (mm):")
-                    # Print 3D positions in Camera Frame
-                    print(pos_camera)
-
-                    print("\nworkspace Frame (mm):")
-                    # Print 3D positions in workspace Frame
-                    print(pos_workspace)
-
-                    print("-"*50)
             
         else:
             # No tags detected
@@ -242,14 +247,127 @@ def main():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             print("\nExiting validation...")
             break
+
+        # -----------------------------------------------------------------
+        # MAINTAIN FIXED TIMESTEP (CRITICAL!)
+        # -----------------------------------------------------------------
+        
+        # Enforce consistent loop timing
+        elapsed = time.time() - start_time
+        if elapsed < dt:
+            time.sleep(dt - elapsed)
     
-    print("\nValidation complete!")
-    
-    print("\nCleaning up...")
+
+    # =====================================================================
+    # CLEANUP
+    # =====================================================================
+    print("\nStopping recording and cleaning up...")
     cap.release()
     cv2.destroyAllWindows()
+    total_time = time.perf_counter() - begining_time
+    print(f"\nTotal execution time: {total_time:.2f} s")
     print("Done!")
+
+    # Trim unused portions of pre-allocated arrays
+    data_time = data_time[:count]
+    data_ee_pos = data_ee_pos[:count, :]
+
+    # TODO: Save all data to pickle file
+    filename='Camera_Tag_Tracking.pkl'
+    # Create dictionary with all collected data and control parameters
+    print(f"\nSaving data to {filename}...")
+    data_dict = {
+    'time': data_time, # Timestamp (s)
+    'pos_current': data_ee_pos, # Current EE position [x,y,z] (mm)
+    }
+
+    # TODO: Write dictionary to pickle file
+    save_to_pickle(data_dict, filename)
+    print("Data saved successfully!")
+
+
+def plot_3D_trajectory_list(ee_pose_list, title):
+        # Convert lists to arrays
+        ee_pose_list = np.array(ee_pose_list)
+
+        # Slice x, y, z, alpha
+        x = ee_pose_list[:,0]
+        y = ee_pose_list[:,1]
+        z = ee_pose_list[:,2]
+        # alpha = ee_pose_list[:,4]
+
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.plot3D(x, y, z)
+        ax.set_title(title)
+        ax.set_xlabel('x (mm)')
+        ax.set_ylabel('y (mm)')
+        ax.set_zlabel('z (mm)')
+        plt.show()
+    
+
+def plot_xyz_posvelacc_list(t_list, ee_pose_list, title):
+        # Convert lists to arrays
+        t_list = np.array(t_list)
+        ee_pose_list = np.array(ee_pose_list)
+
+        # Slice x, y, z
+        x = ee_pose_list[:,0]
+        y = ee_pose_list[:,1]
+        z = ee_pose_list[:,2]
+
+        # Calculate Velocity and Acceleration
+        vx = np.gradient(x)
+        vy = np.gradient(y)
+        vz = np.gradient(z)
+        ax = np.gradient(vx)
+        ay = np.gradient(vy)
+        az = np.gradient(vz)
+        
+        # End-effector pose vs time 
+        fig, axs = plt.subplots(3,1)    # create subplots
+        # Plot position
+        axs[0].plot(t_list, x, label='x position')
+        axs[0].plot(t_list, y, label='y position')
+        axs[0].plot(t_list, z, label='z position')
+        axs[0].set_title("XYZ position vs time")
+        axs[0].set_xlabel("Time (s)")
+        axs[0].set_ylabel("Position (mm)")
+        axs[0].legend(loc='upper right')
+        # Plot velocity
+        axs[1].plot(t_list, vx, label='x velocity')
+        axs[1].plot(t_list, vy, label='y velocity')
+        axs[1].plot(t_list, vz, label='z velocity')
+        axs[1].set_title("XYZ velocity vs time")
+        axs[1].set_xlabel("Time (s)")
+        axs[1].set_ylabel("Velocity (mm/s)")
+        axs[1].legend(loc='upper right')
+        # Plot acceleration
+        axs[2].plot(t_list, ax, label='x acceleration')
+        axs[2].plot(t_list, ay, label='y acceleration')
+        axs[2].plot(t_list, az, label='z acceleration')
+        axs[2].set_title("XYZ accerlcation vs time")
+        axs[2].set_xlabel("Time (s)")
+        axs[2].set_ylabel("Acceleration (mm/s\u00B2)")
+        axs[2].legend(loc='upper right')
+        
+        # Open plot
+        fig.suptitle(title)
+        plt.tight_layout()
+        plt.show()    
+
+def ploting():
+    # Load data from pickle files
+    data_camtrack = load_from_pickle('Camera_Tag_Tracking.pkl')
+    # X step data
+    time_pkl_ = data_camtrack["time"]
+    ee_pose_pkl = data_camtrack["pos_current"]
+
+    plot_3D_trajectory_list(ee_pose_pkl, "3D Trajectory")
+    plot_xyz_posvelacc_list(time_pkl_, ee_pose_pkl, "Position, Velocity, Acceleration")
+
 
 
 if __name__ == "__main__":
-    main()
+    record()
+    ploting()
