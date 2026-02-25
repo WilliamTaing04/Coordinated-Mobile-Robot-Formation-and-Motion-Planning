03b5886af35e6054c7ff38aacce87c9701c4f216
Kyle K <sunhelix248@gmail.com>
2026-02-24 21:47:34 -0800

    Huge changes

--- Jetbot_Setup_2.py
+++ Jetbot_Setup_2.py
@@ -7,173 +7,90 @@ import math
 import AprilTags
 
 class Jetbot():
-    def __init__(self, id, role=0, tau_pose=0.2, tau_vel=0.0):
+    def __init__(self, id, role=0, tau=0.2, tau_vel=0.0):
         self.id = id
         self.role = role                # 0-follower 1-leader
         self.visible = 0                # 0-not seen 1-seen
-        self.time_meas = None           # time of measurement
-        self.prev_time_meas = None      # time of previous measurement
+        self.time_meas = np.zeros((2))           # time of measurement (current, past)
+
         self.pose = None                # [x, y, theta]
-        self.prev_pose = None           # [x, y, theta]
-        self.pose_f = None              # [x, y, theta] (filtered)
-        self.prev_pose_f = None         # [x, y, theta] (filtered)
-        self._yaw_unwrapped = None      # [rad] yaw/theta (unwrapped)
-        self._prev_yaw_raw = None       # [rad] yaw/theta (wrapped)
-        self.lin_vel = None             # mm/s
-        self.prev_lin_vel = 0          # mm/s
-        self.ang_vel = None             # rad/s
-        self.prev_ang_vel = 0          # rad/s
-        self.lin_vel_f = 0.0            # mm/s (filtered)
-        self.ang_vel_f = 0.0            # rad/s (filtered)
-        self._prev_lin_vel_f = 0.0      # mm/s (filtered)
-        self._prev_ang_vel_f = 0.0      # rad/s (filtered)
+        self.pose_f = np.zeros((3,2))              # [x, y, theta] (filtered) (current, past)
+
+        self.lin_vel_x = np.zeros((2))            # x mm/s (filtered)
+        self.lin_vel_y = np.zeros((2))            # y mm/s (filtered)
+        self.lin_vel = None                       # directional velocity
+        self.ang_vel = np.zeros((2))              # theta rad/s (filtered)
+
+        self.lin_acc_x = np.zeros((2))
+        self.lin_acc_y = np.zeros((2))
         self.lin_acc = None             # mm/s^2
-        self.prev_lin_acc = 0           # mm/s^2
-        self.ang_acc = None             # rad/s^2
-        self.prev_ang_acc = 0           # rad/s^2
-        self.tau_pose = tau_pose        # sec
-        self.tau_vel  = tau_vel         # 0 disables extra vel filtering
+
+        self.tau = tau        # sec
 
     def update_meas(self, pose, time_meas):
-        pose = np.asarray(pose, dtype=float).copy()
-        time_meas = float(time_meas)
+        self.pose = np.asarray(pose, dtype=float).copy()
+        self.time_meas = float(time_meas)
+
+        dt = self.time_meas[0] - self.time_meas[1]
+        alpha_pose = dt / (self.tau + dt)
+
         # Check for first update
-        if self.pose is None or self.time_meas is None:
-            self.pose = pose
-            self.prev_pose = pose.copy()
-            self._prev_yaw_raw = pose[2]
-            self._yaw_unwrapped = pose[2]
-            self.pose_f = pose.copy()
-            self.prev_pose_f = pose.copy()
-            self.time_meas = time_meas
-            self.prev_time_meas = time_meas
+        if self.pose is None or self.time_meas is None or dt > 0.2:
+            self.pose_f = [pose.copy() , pose.copy()]
+            self.time_meas = [time_meas, time_meas]
 
             self.lin_vel = 0.0
             self.ang_vel = 0.0
             self.lin_acc = 0.0
-            self.ang_acc = 0.0
             return
         
-        # Update time measured
-        self.prev_time_meas = self.time_meas
-        self.time_meas = time_meas
-        dt = self.time_meas - self.prev_time_meas
-
-        # Check for dt contraints
         if dt <= 1e-6:
             return
-        if dt > 0.2:   # ignore dt > 200ms 
-            # Update pose/time
-            self.prev_pose = self.pose
-            self.pose = pose
-            self.prev_pose_f = self.pose_f.copy()
-            self.pose_f = pose.copy() 
-            self._prev_yaw_raw = pose[2]
-            self._yaw_unwrapped = pose[2]
-            self.lin_vel = 0.0
-            self.ang_vel = 0.0
-            self.lin_vel_f = 0.0
-            self.ang_vel_f = 0.0
-            self.lin_acc = 0.0
-            self.ang_acc = 0.0
-            return
 
-        # Update pose
-        self.prev_pose = self.pose # what is this used for
-        self.prev_lin_vel = self.lin_vel
-        self.prev_ang_vel = self.ang_vel
-        self.prev_pose_f = self.pose_f.copy()
+        # filter x,y, and theta
+        self.pose_f[0,0] = (1 - alpha_pose) * self.pose_f[1,0] + alpha_pose * self.pose[0] # X
+        self.pose_f[0,1] = (1 - alpha_pose) * self.pose_f[1,1] + alpha_pose * self.pose[1] # Y
+        self.pose_f[0,2] = (1 - alpha_pose) * self.pose_f[1,2] + alpha_pose * self.pose[2] # theta
 
-        self.pose = pose 
+        self.lin_vel_x[0] = (self.pose_f[0,0] - self.pose_f[1,0]) / dt # x lin velocity
+        self.lin_vel_y[0] = (self.pose_f[0,1] - self.pose_f[1,1]) / dt # y lin velocity
+        self.ang_vel[0] = (self.pose_f[0,2] - self.pose_f[1,2]) / dt # theta ang velocity (pass in theta as rad)
 
-        # Unwrap yaw
-        dyaw = self.wrap_to_pi(self.pose[2] - self._prev_yaw_raw)
-        self._yaw_unwrapped = self._yaw_unwrapped + dyaw
-        self._prev_yaw_raw = self.pose[2] # what is this for
+        self.lin_vel =  self.lin_vel_x * np.cos(self.pose_f[0,2]) + self.lin_vel_y * np.sin(self.pose_f[0,2])
         
-        # LPF pose
-        alpha_pose = dt / (self.tau_pose + dt) if self.tau_pose > 0 else 1.0
-        
+        self.lin_acc_x[0] = (self.lin_vel_x[0] - self.lin_vel_x[1]) / dt
+        self.lin_acc_y[0] = (self.lin_vel_y[0] - self.lin_vel_y[1]) / dt
+        # add in if we want angular acceleration
 
-        # filter x,y
-        self.pose_f[0] = (1 - alpha_pose) * self.pose_f[0] + alpha_pose * self.pose[0] # X
-        self.pose_f[1] = (1 - alpha_pose) * self.pose_f[1] + alpha_pose * self.pose[1] # Y
-        # filter unwrapped yaw
-        self.pose_f[2] = (1 - alpha_pose) * self.pose_f[2] + alpha_pose * self._yaw_unwrapped # is this correct?
+        self.lin_acc =  self.lin_acc_x * np.cos(self.pose_f[0,2]) + self.lin_acc_y * np.sin(self.pose_f[0,2])
 
-        # Find displacement from last move
-        dx = self.pose_f[0] - self.prev_pose_f[0]
-        dy = self.pose_f[1] - self.prev_pose_f[1]
+        self.pose_f[1,:] = self.pose_f[0,:] # set all current values to be previous values
+        self.lin_vel_x[1] = self.lin_vel_x[0]
+        self.lin_vel_y[1] = self.lin_vel_y[0]
+        self.lin_acc_x[1] = self.lin_acc_x[0]
+        self.lin_acc_y[1] = self.lin_acc_y[0]
 
-        # Calculate velocities using filtered values
-        yaw_prev = self.prev_pose_f[2]
-        self.lin_vel = (dx * np.cos(yaw_prev) + dy * np.sin(yaw_prev)) / dt
-        self.ang_vel = (self.pose_f[2] - self.prev_pose_f[2]) / dt
+        self.time_meas[1] = self.time_meas[0]
 
-        # LPF velocity
-        if self.tau_vel > 0:
-            alpha_vel = dt / (self.tau_vel + dt)
-            self._prev_lin_vel_f = self.lin_vel_f
-            self._prev_ang_vel_f = self.ang_vel_f
-            self.lin_vel_f = (1 - alpha_vel) * self.lin_vel_f + alpha_vel * self.lin_vel
-            self.ang_vel_f = (1 - alpha_vel) * self.ang_vel_f + alpha_vel * self.ang_vel
-            vel_for_acc_lin = self.lin_vel_f
-            vel_for_acc_ang = self.ang_vel_f
-            prev_vel_for_acc_lin = self._prev_lin_vel_f
-            prev_vel_for_acc_ang = self._prev_ang_vel_f
-        else:
-            vel_for_acc_lin = self.lin_vel
-            vel_for_acc_ang = self.ang_vel
-            prev_vel_for_acc_lin = self._prev_lin_vel
-            prev_vel_for_acc_ang = self._prev_ang_vel
-
-        # Diferentiate velocity to get acceleration
-        self.prev_lin_acc = self.lin_acc
-        self.prev_ang_acc = self.ang_acc
-        self.lin_acc = (vel_for_acc_lin - prev_vel_for_acc_lin) / dt
-        self.ang_acc = (vel_for_acc_ang - prev_vel_for_acc_ang) / dt
-
-        return()
+        d = self.pose_f[0,0] * np.cos(self.pose_f[0,2]) + self.pose_f[0,1] * np.sin(self.pose_f[0,2])
+        return d, self.lin_vel, self.pose_f[0,2] # added in for proper inputs to alg
     
-    def get_dist_and_theta(self, agent):
-        # Slice pose
-        x1, y1, theta1 = self.pose
-        x2, y2, theta2 = agent.pose
-
-        # Calculate dist and theta
-        d = np.hypot((x2-x1),(y2-y1))
-        theta = self.wrap_to_pi(theta2 - theta1)
-
-        return d, theta
-        
-
     def reset(self):
-        self.time_meas = None
-        self.prev_time_meas = None
-        self.pose = None
-        self.prev_pose = None
-        self.lin_vel = None
-        self._prev_lin_vel = 0
-        self.ang_vel = None
-        self._prev_ang_vel = 0
-        self.lin_acc = None  
-        self.prev_lin_acc = 0
-        self.ang_acc = None
-        self.prev_ang_acc = 0
-        self.pose_f = None
-        self.prev_pose_f = None
-        self._yaw_unwrapped = None
-        self._prev_yaw_raw = None
-        self.lin_vel_f = 0.0
-        self.ang_vel_f = 0.0
-        self._prev_lin_vel_f = 0.0
-        self._prev_ang_vel_f = 0.0
+        self.time_meas = np.zeros((2))           # time of measurement (current, past)
 
-    def wrap_to_pi(self, a: float) -> float:
-        # Wrap angle to [-pi, pi]
-        return (a + np.pi) % (2 * np.pi) - np.pi
-        
-        
+        self.pose = None                # [x, y, theta]
+        self.pose_f = np.zeros((3,2))              # [x, y, theta] (filtered) (current, past)
+
+        self.lin_vel_x = np.zeros((2))            # x mm/s (filtered)
+        self.lin_vel_y = np.zeros((2))            # y mm/s (filtered)
+        self.lin_vel = None                       # directional velocity
+        self.ang_vel = np.zeros((2))              # theta rad/s (filtered)
+
+        self.lin_acc_x = np.zeros((2))
+        self.lin_acc_y = np.zeros((2))
+        self.lin_acc = None             # mm/s^2
+
+
 
 def camera_setup(width=1280, height=720, fps=100):
     print("\nInitializing camera and detector...")
