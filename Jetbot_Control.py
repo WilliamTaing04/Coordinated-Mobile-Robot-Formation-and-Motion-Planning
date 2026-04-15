added x y threshold scaling for object avoidance, fixed relavtive object measurment bugs, controller x and y edge now match jetbot x and y edge, object avoidance needs tuning(thersholds and weights)

Jetbot_Control.py
@@ -92,15 +92,15 @@ python3 -m jetbot.control_reciever
 
     # Jetbots
     leader = Jetbot_Setup.Jetbot(26,"10.40.109.62",controllerL, None, None, role=1,tau_pose=0.01,tau_vel=0.01)   # TagID, 0-follower
-    follower1 = Jetbot_Setup.Jetbot(61,"10.40.122.89",controller1, leader, leader, role=0,tau_pose=0.0075,tau_vel=0.0075)   # TagID, 0-follower
-    follower2 = Jetbot_Setup.Jetbot(9,"10.40.122.94",controller2, leader, follower1, role=0,tau_pose=0.0075,tau_vel=0.0075)   # TagID, 0-follower
-    obstacle1 = Jetbot_Setup.Jetbot(11,"10.40.122.8",controllerobs, None, None, role=2,tau_pose=0.0075,tau_vel=0.0075, radius=0.1)   #TODO: check ip and tag id
+    follower1 = Jetbot_Setup.Jetbot(11,"10.40.101.192",controller1, leader, leader, role=0,tau_pose=0.0075,tau_vel=0.0075)   # TagID, 0-follower
+    follower2 = Jetbot_Setup.Jetbot(9,"10.40.122.94",controller2, leader, leader, role=0,tau_pose=0.0075,tau_vel=0.0075)   # TagID, 0-follower
+    obstacle1 = Jetbot_Setup.Jetbot(61,"10.40.122.89",controllerobs, None, None, role=2,tau_pose=0.0075,tau_vel=0.0075, radius=0.1)
     # follower3 = Jetbot_Setup.Jetbot(9994,"10.40.122.89",controller4,role=0,tau_pose=0.1,tau_vel=0.1)   # TagID, 0-follower
 
     # Controller params: x_id, y_id, ds_x, ds_y, dsafe_y, gd TODO: state may have to be measured at init
     agentL = None
     agent1 = agent.Agent([0,0,0,0], 1, 0, 0, 3, [-4, -0.5, -0.5], controller.SafeObstacleAvoidanceController(np.array([0, 0, 0.3, -0.3, -0.05,-4])))
-    agent2 = agent.Agent([0,0,0,0], 2, 0, 1, 3, [-4, -0.5, -0.5], controller.SafeObstacleAvoidanceController(np.array([0, 0, 0.3, 0.3, 0.05,-4])))
+    agent2 = agent.Agent([0,0,0,0], 2, 0, 0, 3, [-4, -0.5, -0.5], controller.SafeObstacleAvoidanceController(np.array([0, 0, 0.3, 0.3, 0.05,-4])))
     agentobst1 = None
     #agent params: state, id, xid, yid, cluster size, estimator gains (gd, gv, p), controller,
     # Jetbot/Agent Arrays
@@ -114,15 +114,10 @@ python3 -m jetbot.control_reciever
     #                    [150, 0.3, 3], 
     #                    [0.0, 0.0, 10]]
 
-    leader_movement = [[150, 0.0, 3], 
-                       [150, 0.0, 3], 
-                       [150, 0.0, 3], 
-                       [150, 0.0, 0], 
-                       [0.0, 1.0, 0]]
+    leader_movement = [[125.0, 0.0, 12],
+                       [0.0, 0.0, 100]]
     
-    obstacle1_movement = [[0.0, 0.0, 2], 
-                        [0.0, 0.0, 3],
-                        [0.0, 0.0, 10]]
+    obstacle1_movement = [[0.0, 0.0, 2]]
     
     leader_move = 0
     obstacle1_move = 0
@@ -225,7 +220,6 @@ python3 -m jetbot.control_reciever
 
                                     if isinstance(agent_array[i].controller, controller.SafeObstacleAvoidanceController):
                                         #UPDATE OBSTACLES:
-                                        #print(f"Jetbot{jetbot.id}: {jetbot.get_obst_meas(jetbot_array)}")
                                         agent_array[i].controller.obstacle_data = jetbot.get_obst_meas(jetbot_array)
 
                     # Reduce display output
@@ -263,10 +257,8 @@ python3 -m jetbot.control_reciever
                     t_now = time.perf_counter()
                     # UW controller
                     v_cmd , w_cmd = jetbot.controller.controller_uw([jetbot.lin_vel_f, jetbot.ang_vel_f],[U_GOAL*1000, W_GOAL])
-                    print("jetbot:",jetbot.id, "v:", v_cmd, "w:", w_cmd)
                     # Convert Desired VW to LR motor speed
                     left, right = jetbot.controller.motor_controller(v_cmd, w_cmd)
-                    print("jetbot:",jetbot.id, "l:", left, "r:", right)
                 
                 elif jetbot.visible and jetbot.role==1: # For leaders
                     if leader_move < len(leader_movement):
@@ -429,4 +421,4 @@ def plots():
 
 if __name__ == "__main__":
     main()
-    # plots()
+    #plots()

Jetbot_Setup.py
@@ -155,22 +155,25 @@ class Jetbot():
 
         for obstacle in jetbot_array:
             if obstacle.role == 2:    # check for obstacle
+                R_g2a = np.array([[np.cos(self.pose_f[2]), np.sin(self.pose_f[2])], 
+                                  [-np.sin(self.pose_f[2]),  np.cos(self.pose_f[2])]])
+                
                 obs_radius = obstacle.radius
-                obs_x, obs_y, obs_heading = obstacle.pose_f
+                obs_x_glob, obs_y_glob, obs_heading = obstacle.pose_f
+                # Relative distance between obstacle and agent in agent's frame
+                obs_x_ag, obs_y_ag = R_g2a @ np.array([obs_x_glob - self.pose_f[0], obs_y_glob - self.pose_f[1]])
+                # Relative (magnitude and direction) velocity between obstacle and agent in global frame
+                v_obs_x_glob = (obstacle.lin_vel_f * np.cos(obs_heading)) - (self.lin_vel_f * np.cos(self.pose_f[2]))
+                v_obs_y_glob = (obstacle.lin_vel_f * np.sin(obs_heading)) - (self.lin_vel_f * np.sin(self.pose_f[2]))
+                # Relative (magnitude and direction) velocity between obstacle and agent in agent's frame
+                v_obs_x_ag, v_obs_y_ag = R_g2a @ np.array([v_obs_x_glob, v_obs_y_glob])
+                # Predicted distance between obstacle and agent in agent's frame after one time step
+                pred_obs_x_rel = obs_x_ag + v_obs_x_ag * dt
+                pred_obs_y_rel = obs_y_ag + v_obs_y_ag * dt
 
-                v_obs_x = obstacle.lin_vel_f * np.cos(obs_heading)
-                v_obs_y = obstacle.lin_vel_f * np.sin(obs_heading)
+                theta_rel = np.atan2(pred_obs_y_rel, pred_obs_x_rel) #(obs_heading - self.pose_f[2] + np.pi) % (2 * np.pi) - np.pi
 
-                pred_obs_x = obs_x + v_obs_x * dt
-                pred_obs_y = obs_y + v_obs_y * dt
-
-                d_obs_x = pred_obs_x - self.pose_f[0]
-                d_obs_y = pred_obs_y - self.pose_f[1]
-
-                theta_obs = np.atan2(d_obs_y, d_obs_x)
-                theta_rel = (theta_obs - self.pose_f[2] + np.pi) % (2 * np.pi) - np.pi
-
-                obst_meas.append([v_obs_x / 1000, v_obs_y / 1000, d_obs_x / 1000, d_obs_y / 1000, theta_rel, obs_radius])   # array for [i] object
+                obst_meas.append([v_obs_x_ag / 1000, v_obs_y_ag / 1000, pred_obs_x_rel / 1000, pred_obs_y_rel / 1000, theta_rel, obs_radius])   # array for [i] object, TODO: Check for non-predictd relative positions performance
 
         return obst_meas
 